e6d8375cb2b9ca5d5cc8a2615492e4c3d01c6b77
kirk <kirk@benchmarkthing.com>
2025-02-01 19:45:57 -0800

    feat: suppport better env management

---
+++ tests/test_webarena.py
@@ -0,0 +1,18 @@
+from benchflow import load_benchmark
+from webarena_openai import WebarenaAgent
+import os
+
+bench = load_benchmark(benchmark_name="webarena")
+
+your_agents = WebarenaAgent()
+
+run_ids = bench.run(
+    task_ids=[0],
+    agents=your_agents,
+    api={"OPENAI_API_KEY": os.getenv("OPENAI_API_KEY")},
+    requirements_dir="webarena_requirements.txt",
+    params={}
+)
+
+results = bench.get_results(run_ids)
+print(results)
