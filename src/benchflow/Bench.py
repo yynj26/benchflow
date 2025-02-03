082f4a6be68e70d6dedbffa7015d667be8c0bd4b
kirk <kirk@benchmarkthing.com>
2025-02-03 00:18:07 -0800

    feat: reduce redundent agent delopy

--- src/benchflow/Bench.py
+++ src/benchflow/Bench.py
@@ -1,26 +1,33 @@
-import sys
 import base64
-import time
-import requests
-from typing import List, Union, Dict, Any
 import logging
+import sys
+import time
+from typing import Any, Dict, List, Union
+
+import requests
+from requests.exceptions import HTTPError
+
 from .BaseAgent import BaseAgent
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+# utility function to encode content to base64
+def encode_base64(content: str) -> str:
+    return base64.b64encode(content.encode()).decode() if content else None
 
 class Bench:
     def __init__(self, benchmark: Dict[str, Any]):
         self.benchmark_url = benchmark["benchmark_url"]
         self.benchmark_name = benchmark["benchmark_name"]
         self.resource_manager_url = "http://159.89.229.132:10000"
-        self.running_tasks = {}
+        self.running_agents = {}
         self.results = {}
         
     def run(self, task_ids: Union[str|int, List[str|int]], 
             agents: Union[BaseAgent, List[BaseAgent]], 
             requirements_dir: str, 
-            install_sh_dir: str = None, 
+            install_sh: str = None, 
             api: Dict[str, str] = None, 
             require_gpu: bool = False, 
             params: Dict[str, Any] = {}):
@@ -29,61 +36,77 @@ class Bench:
             task_ids = [str(task_ids)]
         if isinstance(agents, BaseAgent):
             agents = [agents]
-
+        
         results_ids = []
-        for task_id in task_ids:
-            task_id = str(task_id)
-            for Baseagent in agents:
-                results_ids.append(self._run_single_task(task_id, Baseagent, require_gpu, requirements_dir, install_sh_dir, api, params))
-        self.cleanup()
+        for agent in agents:
+            agent_url = self._deploy_agent(agent, require_gpu, requirements_dir, install_sh, api)
+            if not agent_url:
+                logger.error(f"Deployment failed on {agent.__class__.__name__}")
+                continue
+            for task_id in task_ids:
+                task_id = str(task_id)
+                results_ids.append(self._run_single_task(task_id, agent_url, agent, params))
+            self._cleanup()
         return results_ids
 
     def get_results(self, run_ids: List[str]):
         return [self.results[run_id] for run_id in run_ids]
 
-    def _run_single_task(self, task_id: str, 
-                         Baseagent: BaseAgent, 
-                         require_gpu: bool, 
-                         requirements_dir: str, 
-                         install_sh_dir: str, 
-                         api: Dict[str, str] = None, 
-                         params: Dict[str, Any] = {}):
-        
-        logger.info(f"Starting task {task_id} on {Baseagent.__class__.__name__}")
-        
+    def _deploy_agent(self, 
+                      agent: BaseAgent, 
+                      require_gpu: bool, 
+                      requirements_dir: str, 
+                      install_sh_dir: str, 
+                      api: Dict[str, str] = None):
+        logger.info(f"Starting deployment of agent {agent.__class__.__name__}")
         try:
             with open(requirements_dir, 'r') as f:
                 requirements_txt = f.read()
-            with open(install_sh_dir, 'r') as f:
-                install_sh = f.read()
-            agent_code = self._get_agent_code(Baseagent)
-            response = requests.post(
-                f"{self.resource_manager_url}/deploy",
-                json={
-                    "agent_code": base64.b64encode(agent_code.encode()).decode(),
-                    "require_gpu": require_gpu,
-                    "requirements_txt": base64.b64encode(requirements_txt.encode()).decode(),
-                    "install_sh": base64.b64encode(install_sh.encode()).decode() if install_sh else None,
-                    "benchmark_name": self.benchmark_name,
-                    "api": api
-                }
-            )
+
+            install_sh = None
+            if install_sh_dir:
+                with open(install_sh_dir, 'r') as f:
+                    install_sh = f.read()
+
+            agent_code = self._get_agent_code(agent)
+            payload = {
+                "agent_code": encode_base64(agent_code),
+                "require_gpu": require_gpu,
+                "requirements_txt": encode_base64(requirements_txt),
+                "install_sh": encode_base64(install_sh),
+                "benchmark_name": self.benchmark_name,
+                "api": api
+            }
+
+            response = requests.post(f"{self.resource_manager_url}/deploy", json=payload)
             response.raise_for_status()
-            logger.info(f"{Baseagent.__class__.__name__} deployed successfully on port {response.json()['port']}")
-            
+
             deploy_info = response.json()
             host, port = deploy_info["host"], deploy_info["port"]
             agent_url = f"http://{host}:{port}"
 
+            self.running_agents[str(agent.__class__.__name__)] = {"host": host, "port": port, "start_time": time.time()}
+            logger.info(f"{agent.__class__.__name__} deployed successfully on port {port}")
 
-            self.running_tasks[task_id] = {
-                "host": host,
-                "port": port,
-                "start_time": time.time()
-            }
+            return agent_url
+        
+        except HTTPError as e:
+            logger.error(f"Deployment failed: {str(e)}")
+            error_detail = response.json()['detail']
+            logger.error(f"Deployment failed: {error_detail}")
+        except Exception as e:
+            logger.error(f"Deployment failed: {str(e)}")
+        return False
 
+    def _run_single_task(self, task_id: str,
+                         agent_url: str, 
+                         agent: BaseAgent, 
+                         params: Dict[str, Any] = {}):
+        
+        logger.info(f"Starting task {task_id} on {agent.__class__.__name__}")
+        
+        try:
             url = f"{self.benchmark_url}/api/v1/{self.benchmark_name}/evaluate"
-
             response = requests.post(
                 url,
                 json={
@@ -92,8 +115,8 @@ class Bench:
                     "params": params
                 }
             )
-
-            logger.info(f"Task {task_id} on {Baseagent.__class__.__name__} finished")
+            response.raise_for_status()
+            logger.info(f"Task {task_id} on {agent.__class__.__name__} finished")
             self.results[task_id] = response.json()
             return task_id
         
@@ -104,23 +127,23 @@ class Bench:
             self.results[task_id] = "failed"
             return task_id
         
-    def _get_agent_code(self, Baseagent: BaseAgent) -> str:
-        agent_file = sys.modules[Baseagent.__class__.__module__].__file__
+    def _get_agent_code(self, agent: BaseAgent) -> str:
+        agent_file = sys.modules[agent.__class__.__module__].__file__
         with open(agent_file, 'r') as f:
             return f.read()
 
-    def cleanup(self):
-        for task_id, task_info in self.running_tasks.items():
-            logger.info(f"Releasing task {task_id} on {task_info['host']}:{task_info['port']}")
+    def _cleanup(self):
+        for agent_name, agent_info in self.running_agents.items():
+            logger.info(f"Releasing agent {agent_name} on {agent_info['host']}:{agent_info['port']}")
             try:
                 response = requests.post(
                     f"{self.resource_manager_url}/release",
                     json={
-                        "host": task_info["host"],
-                        "port": task_info["port"]
+                        "host": agent_info["host"],
+                        "port": agent_info["port"]
                     }
                 )
                 response.raise_for_status()
-                logger.info(f"Release successfully for task {task_id} on {task_info['host']}:{task_info['port']}")
+                logger.info(f"Release successfully for agent {agent_name} on {agent_info['host']}:{agent_info['port']}")
             except Exception as e:
-                logger.error(f"Release request failed for task {task_id} on {task_info['host']}:{task_info['port']}: {str(e)}")
+                logger.error(f"Release request failed for agent {agent_name} on {agent_info['host']}:{agent_info['port']}: {str(e)}")
