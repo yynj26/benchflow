297e7be1c28ef1f391ff62eb81a8640b61ad23d2
kirk <kirk@benchmarkthing.com>
2025-02-03 00:18:38 -0800

    test: add multi task test for webarena

--- tests/test_webarena.py
+++ tests/test_webarena.py
@@ -9,7 +9,7 @@ bench = load_benchmark(benchmark_name="webarena")
 your_agents = WebarenaAgent()
 
 run_ids = bench.run(
-    task_ids=[0],
+    task_ids=[0,1],
     agents=your_agents,
     api={"OPENAI_API_KEY": os.getenv("OPENAI_API_KEY")},
     requirements_dir="webarena_requirements.txt",
