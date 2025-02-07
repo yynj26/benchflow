78ecf36fc665d584655b33ed9797c00173521458
kirk <kirk@benchmarkthing.com>
2025-02-06 20:17:10 -0800

    feat: abstract the interface for benchmark developers

--- src/benchflow/__init__.py
+++ src/benchflow/__init__.py
@@ -2,8 +2,8 @@ from .BaseAgent import BaseAgent
 from .Bench import Bench
 from .BenchClient import BenchClient
 from .load_benchmark import load_benchmark
-from .BaseBench import BaseBench
+from .BaseBench import BaseBench, BaseBenchConfig
 
 __version__ = "0.1.5"
 
-__all__ = ["Bench", "BaseAgent", "BenchClient", "load_benchmark", "BaseBench"]
+__all__ = ["Bench", "BaseAgent", "BenchClient", "load_benchmark", "BaseBench", "BaseBenchConfig"]
